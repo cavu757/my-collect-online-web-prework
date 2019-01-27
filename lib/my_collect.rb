c00d4c6fd39d382c366e7552688a03cb0efac350
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-27 03:20:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,13 @@
 def my_collect(array)
   i = 0
-  new_array = []
+#  new_array = []
   while i < array.length
     yield(array[i])
     puts array[i]
-    new_array << array[i]
-    puts new_array
+#    new_array << array[i]
+#    puts new_array
     i = i + 1
   end
-  new_array
+#  new_array
 end
 
